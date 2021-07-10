b5687ecd047155a7253dbeb82e7c9db6d242054f
arunagi <aldrinthomaslab@gmail.com>
2021-07-10 21:25:02 +0800

    Feature: Add goal and accounts component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,74 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import {
+  Container,
+  Row,
+  Col,
+  Card
+} from 'react-bootstrap';
+
+import {
   PageWrapper,
 } from './../../components';
 
+import './style.css';
+
 const Home = () => (
   <PageWrapper>
-    <div>Sample</div>
-    <Link to="/account">Accounts</Link>
+    <Container>
+      <Row>
+        <Col className="home__section">
+          <h2>Goal</h2>
+          <h1>Php 0.00</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="home__section">
+          <h2>Accounts</h2>
+          <div>
+            <Container>
+              <Row>
+                <Col xs={4}>
+                  <Card>
+                    <Card.Body>
+                      <h4>ACC</h4>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col xs={4}>
+                  <Card>
+                    <Card.Body>
+                      <h4>ACC</h4>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col xs={4}>
+                  <Card>
+                    <Card.Body>
+                      <h4>ACC</h4>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col xs={4}>
+                  <Card>
+                    <Card.Body>
+                      <h4>ACC</h4>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col xs={4}>
+                  <Card>
+                    <Card.Body>
+                      <h4>ACC</h4>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Row>
+            </Container>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   </PageWrapper>
 );
 
