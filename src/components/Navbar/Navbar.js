f65814723fb9c62b4bcaf510bd638b60a40a97a0
arunagi <aldrinthomaslab@gmail.com>
2021-07-10 19:55:15 +0800

    Feature: Create Navbar component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  Navbar as ReactNavbar,
+  Container,
+  Nav
+} from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap'
+
+const Navbar = () => (
+  <ReactNavbar bg="dark">
+    <Container>
+      <LinkContainer to="/">
+        <ReactNavbar.Brand>Budj</ReactNavbar.Brand>
+      </LinkContainer>
+      <ReactNavbar.Toggle aria-controls="basic-navbar-nav" />
+      <ReactNavbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <LinkContainer to="/">
+            <Nav.Link>Home</Nav.Link>
+          </LinkContainer>
+        </Nav>
+      </ReactNavbar.Collapse>
+    </Container>
+  </ReactNavbar>
+);
+
+export default Navbar;
--- src/components/index.js
+++ src/components/index.js
@@ -2,11 +2,12 @@ import ExpenseForm from './ExpenseForm/ExpenseForm';
 import PageWrapper from './PageWrapper/PageWrapper';
 import ExpenseTable from './ExpenseTable/ExpenseTable';
 import ExpenseModal from './ExpenseModal/ExpenseModal';
+import Navbar from './Navbar/Navbar';
 
 export {
   ExpenseForm,
   PageWrapper,
   ExpenseTable,
-  ExpenseModal
+  ExpenseModal,
+  Navbar
 };
-
