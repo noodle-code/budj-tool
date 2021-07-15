b1698ad96f9df0883ec026e9e35b451e492b99d1
arunagi <aldrinthomaslab@gmail.com>
2021-07-15 21:29:51 +0800

    Feature: Map accounts to component list

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -12,46 +12,58 @@ import {
 
 import './style.css';
 
-const Home = () => (
-  <PageWrapper>
-    <Container>
-      <Row>
-        <Col className="home__section">
-          <h2>Goal</h2>
-          <h1>Php 0.00</h1>
-        </Col>
-      </Row>
-      <Row>
-        <Col className="home__section">
-          <div className="home__section__header">
-            <h2>Accounts</h2>
-            <p>Manage</p>
-          </div>
-          <div>
-            <Container>
-              <Row>
-                <Col xs={4}>
-                  <AccountCard to="/account" name="ACC" />
-                </Col>
-                <Col xs={4}>
-                  <AccountCard to="/account" name="ACC" />
-                </Col>
-                <Col xs={4}>
-                  <AccountCard to="/account" name="ACC" />
-                </Col>
-                <Col xs={4}>
-                  <AccountCard to="/account" name="ACC" />
-                </Col>
-                <Col xs={4}>
-                  <AccountCard to="/account" name="ACC" />
-                </Col>
-              </Row>
-            </Container>
-          </div>
-        </Col>
-      </Row>
-    </Container>
-  </PageWrapper>
-);
+const accounts = [
+  {
+    id: 1,
+    name: 'ACC',
+    currentAmount: 10000
+  },
+  {
+    id: 1,
+    name: 'ACC',
+    currentAmount: 10000
+  },
+  {
+    id: 1,
+    name: 'ACC',
+    currentAmount: 10000
+  }
+
+];
+
+const Home = () => {
+  return (
+    <PageWrapper>
+      <Container>
+        <Row>
+          <Col className="home__section">
+            <h2>Goal</h2>
+            <h1>Php 0.00</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="home__section">
+            <div className="home__section__header">
+              <h2>Accounts</h2>
+              <p>Manage</p>
+            </div>
+            <div className="home__accounts">
+              {accounts.map(item => {
+                return (
+                  <AccountCard
+                    accountId={item.id}
+                    name={item.name}
+                    current={item.currentAmount}
+                  />
+                );
+              })
+              }
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </PageWrapper>
+  );
+}
 
 export default Home;
