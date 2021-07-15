6bd180b3fc5eac10893c4194e21783f873aaf3f2
arunagi <aldrinthomaslab@gmail.com>
2021-07-15 21:28:28 +0800

    Feature: Replace to prop with accountId

--- src/components/AccountCard/AccountCard.js
+++ src/components/AccountCard/AccountCard.js
@@ -8,7 +8,7 @@ import CardLink from './../CardLink/CardLink';
 import './style.css';
 
 const AccountCard = props => (
-  <CardLink to={props.to} className="accountCard">
+  <CardLink to={`/accounts/${props.accountId}`} className="accountCard">
     <Card.Body>
       <h4>{props.name}</h4>
       <p>{props.current}</p>
