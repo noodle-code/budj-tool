f8a6ace97af78c6b812607f811aa01778fdc9729
arunagi <aldrinthomaslab@gmail.com>
2021-07-18 20:38:58 +0800

    Feature: Add style too Accounts page

--- src/pages/Accounts/Accounts.js
+++ src/pages/Accounts/Accounts.js
@@ -10,13 +10,73 @@ import {
   AccountCard
 } from './../../components';
 
+import './style.css';
+
+const accounts = [
+  {
+    id: 1,
+    name: 'ACC',
+    currentAmount: 10000
+  },
+  {
+    id: 1,
+    name: 'ACC',
+    currentAmount: 10000
+  },
+  {
+    id: 1,
+    name: 'ACC',
+    currentAmount: 10000
+  },
+  {
+    id: 1,
+    name: 'ACC',
+    currentAmount: 10000
+  },
+  {
+    id: 1,
+    name: 'ACC',
+    currentAmount: 10000
+  },
+  {
+    id: 1,
+    name: 'ACC',
+    currentAmount: 10000
+  },
+  {
+    id: 1,
+    name: 'ACC',
+    currentAmount: 10000
+  },
+  {
+    id: 1,
+    name: 'ACC',
+    currentAmount: 10000
+  },
+];
+
 const Accounts = () => {
   return (
     <PageWrapper>
       <Container>
         <Row>
-          <Col>
-            Accounts
+          <Col className="accounts__header">
+            <h3>Accounts</h3>
+            <p>Add new</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="accounts__list">
+            {accounts.map(item => {
+              return (
+                <AccountCard
+                  accountId={item.id}
+                  name={item.name}
+                  current={item.currentAmount}
+                />
+              );
+            })
+            }
           </Col>
         </Row>
       </Container>
